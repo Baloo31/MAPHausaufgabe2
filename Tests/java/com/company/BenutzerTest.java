e78cdeefa187ad6be6c9ddf078957a99110994cf
Andrei Balu <78239922+Baloo31@users.noreply.github.com>
2021-10-17 18:27:35 +0300

    Fourth commit: - Tests in einem separeten Folder geschrieben

--- Tests/java/com/company/BenutzerTest.java
+++ Tests/java/com/company/BenutzerTest.java
@@ -1,4 +1,4 @@
-package Tests;
+package com.company;
 
 import Sports.Leichtathletik.Hindernislauf;
 import Sports.Leichtathletik.Hochsprung;
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,7 +1,12 @@
 package com.company;
 
+import Sports.Mannschaftssport.Fussball;
+import Sports.Sport;
+
 public class Main {
 
     public static void main(String[] args) {
+        Sport fussball = new Fussball();
+        fussball.kalkuliereZeit();
     }
 }
