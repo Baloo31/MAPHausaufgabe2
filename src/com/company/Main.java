cb27e79ad04b132a93f41e85c5eb2eb3661ab9ed
Andrei Balu <78239922+Baloo31@users.noreply.github.com>
2021-10-19 14:33:51 +0300

    Fifth commit: - Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,12 +1,35 @@
 package com.company;
 
+import Sports.Leichtathletik.Hochsprung;
 import Sports.Mannschaftssport.Fussball;
 import Sports.Sport;
+import User.Benutzer;
+import org.junit.jupiter.api.Assertions;
+
+import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) {
+
+        // Zwei Sport Sessionen
         Sport fussball = new Fussball();
-        fussball.kalkuliereZeit();
+        Sport hochsprung = new Hochsprung();
+
+        // Ein Benutzer
+        Benutzer benutzer = new Benutzer("Andrei","Balu",Arrays.asList(fussball, hochsprung));
+
+        // Durchschnittzeit fur treiben der Sports aus seiner Stundenplan
+        try{
+            System.out.println(benutzer.kalkuliereDurchschnittzeit());
+        } catch (Exception exception){
+            Assertions.fail();
+        }
+
+        // Die totale Zeit die er fur treiben der Sports aus der Stundenplan braucht
+        System.out.println(benutzer.kalkuliereZeit());
+
+        // Die Zeit die er fur treiben einer Fussball Session braucht
+        System.out.println(benutzer.kalkuliereZeit(fussball));
     }
 }
